d2e1ed308800d5dd76d6b8ff2089df7475a53504
carlacampas <ccg.campas@gmail.com>
2022-03-14 13:51:00 +0100

    add info to intents

--- actions/actions.py
+++ actions/actions.py
@@ -31,5 +31,5 @@ from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 
 class MyKnowledgeBaseAction(ActionQueryKnowledgeBase):
     def __init__(self):
-        knowledge_base = InMemoryKnowledgeBase("rasa_knowledge_base.json")
+        knowledge_base = InMemoryKnowledgeBase("knowledge_base.json")
         super().__init__(knowledge_base) 
