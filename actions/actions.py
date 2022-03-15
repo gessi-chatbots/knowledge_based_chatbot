72195b53ef7a26401d90c4a012b8fb5e2fcad5a4
carlacampas <ccg.campas@gmail.com>
2022-03-15 10:09:35 +0100

    initial knowledge base works

--- actions/actions.py
+++ actions/actions.py
@@ -31,7 +31,5 @@ from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 
 class MyKnowledgeBaseAction(ActionQueryKnowledgeBase):
     def __init__(self):
-        knowledge_base = InMemoryKnowledgeBase("knowledge_base.json")
-        print(knowledge_base.get_objects())
+        knowledge_base = InMemoryKnowledgeBase("rasa_knowledge_base.json")
         super().__init__(knowledge_base) 
-  
