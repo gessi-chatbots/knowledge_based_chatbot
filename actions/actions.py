6a9a1dba39231170832a3435a2e58c4b0429947b
carlacampas <ccg.campas@gmail.com>
2022-03-15 09:32:22 +0100

    knowledge base

--- actions/actions.py
+++ actions/actions.py
@@ -32,4 +32,6 @@ from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 class MyKnowledgeBaseAction(ActionQueryKnowledgeBase):
     def __init__(self):
         knowledge_base = InMemoryKnowledgeBase("knowledge_base.json")
+        print(knowledge_base.get_objects())
         super().__init__(knowledge_base) 
+  
