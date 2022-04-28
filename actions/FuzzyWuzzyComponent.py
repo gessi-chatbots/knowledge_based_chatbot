76f02efb00e58c7b124451383aefe53180c145b2
carlacampas <ccg.campas@gmail.com>
2022-04-28 12:31:26 +0200

    trains but fails at shell

--- actions/FuzzyWuzzyComponent.py
+++ actions/FuzzyWuzzyComponent.py
@@ -1,4 +1,4 @@
-'''
+import typing
 from typing import Dict, Text, Any, List, Optional
 
 from rasa.engine.graph import GraphComponent, ExecutionContext
@@ -7,24 +7,26 @@ from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
 from rasa.shared.nlu.training_data.message import Message
 from rasa.shared.nlu.training_data.training_data import TrainingData
-from rasa.nlu.extractors.extractor import EntityExtractor
+from rasa.nlu.extractors.extractor import EntityExtractorMixin
+from rasa.shared.nlu.constants import ENTITIES
 
-import json
-import os
+import fuzzywuzzy
+
+from actions.KnowledgeBase import KnowledgeBase
+
+# TODO: Correctly register your component with its type
 @DefaultV1Recipe.register(
-    [DefaultV1Recipe.ComponentType.ENTITY_EXTRACTOR], is_trainable=True
+    [DefaultV1Recipe.ComponentType.ENTITY_EXTRACTOR], is_trainable=False
 )
-class FuzzyWuzzyComponent(EntityExtractor, GraphComponent):
-    def __init__(
-        self,
-        model_storage: ModelStorage,
-        resource: Resource,
-        training_artifact: Optional[Dict],
-    ) -> None:
-        # Store both `model_storage` and `resource` as object attributes to be able
-        # to utilize them at the end of the training
-        self._model_storage = model_storage
-        self._resource = resource
+class FuzzyWuzzyComponent(GraphComponent, EntityExtractorMixin):
+    def __init__(self, component_config: Optional[Dict[Text, Any]] = None) -> None:
+        kb = KnowledgeBase()
+        super().__init__(component_config)
+        self.score_cutoff = component_config.get(
+            "score_cutoff", self.defaults["score_cutoff"]
+        )
+    
+    defaults = {"score_cutoff": 80}
 
     @classmethod
     def create(
@@ -34,63 +36,36 @@ class FuzzyWuzzyComponent(EntityExtractor, GraphComponent):
         resource: Resource,
         execution_context: ExecutionContext,
     ) -> GraphComponent:
-        return cls(model_storage, resource, training_artifact=None)
+        return cls(config)
 
-    @classmethod
-    def load(
-        cls,
-        config: Dict[Text, Any],
-        model_storage: ModelStorage,
-        resource: Resource,
-        execution_context: ExecutionContext,
-        **kwargs: Any,
-    ) -> FuzzyWuzzyComponent:
-        try:
-            with model_storage.read_from(resource) as directory_path:
-                with open(directory_path / "artifact.json", "r") as file:
-                    training_artifact = json.load(file)
-                    return cls(
-                        model_storage, resource, training_artifact=training_artifact
-                    )
-        except ValueError:
-            print("no persisted data for your component")
-
-
-    def train(self, training_data: TrainingData) -> Resource:
-        # TODO: Implement this if your component requires training
-        
-        # Persist your graph component
-        with self._model_storage.write_to(self._resource) as directory_path:
-            with open(directory_path / "artifact.json", "w") as file:
-                json.dump({"my": "training artifact"}, file)
+    @staticmethod
+    def required_packages() -> List[Text]:
+        """Any extra python dependencies required for this component to run."""
+        return ["fuzzywuzzy"]
 
     def process_training_data(self, training_data: TrainingData) -> TrainingData:
-        # TODO: Implement this if your component augments the training data with
-        #       tokens or message features which are used by other components
-        #       during training.
-
+        """Processes the training examples in the given training data in-place."""
+        self.process(training_data.training_examples)
         return training_data
 
     def process(self, messages: List[Message]) -> List[Message]:
+        """Processes incoming message and compute and set features."""
+        for message in messages:
+            entities = message.get(ENTITIES, [])
+            message.set(ENTITIES, self._fix_typo(entities), add_to_output=True)
         return messages
-    
-    def process(self, message: Message, **kwargs: Any) -> None:
-        metadata = message.get("metadata")
-        print(metadata.get("intent"))
-        print(metadata.get("example"))
 
-    def persist(self, file_name: Text, model_dir: Text) -> Optional[Dict[Text, Any]]:
-        """Persist this component to disk for future loading."""
+    def _fix_typo(self, entities: List[Dict[Text, Any]]) -> List[Dict[Text, Any]]:
+        entities_fixed = []
+        for entity in entities:
+            if entity['entity'] != 'features': continue
+            value = str(entity["value"])
+            match = fuzzywuzzy.process.extractOne(
+                value, self.kb.getFeatures(), score_cutoff=self.score_cutoff
+            )
+            if match:
+                entity["value"] = match[0]
 
-        if self.entities:
-            file_name = file_name + ".json"
-            entity_file = os.path.join(model_dir, file_name)
-            write_json_to_file(entity_file, self.entities)
-
-            return {"file": file_name}
-        else:
-            return {"file": None}
-
-        # n-grams
-        # look for library
-'''
+            entities_fixed.append(entity)
+        
+        return entities_fixed
